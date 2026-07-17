90a8dca54ce7e4fd78938a42322394231b477115
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-17 17:14:09 +0200

    Dataset: Products - created

---
+++ src/data/products.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: 1,
+    name: "Organic Avocados",
+    category: "Fresh Fruits",
+    price: 4.99,
+    image: "https://images.unsplash.com/photo-1523049673857-eb18f1d7b578?w=60",
+  },
+  {
+    id: 2,
+    name: "Fresh Strawberries",
+    category: "Seasonal Fruits",
+    price: 5.49,
+    image: "https://images.unsplash.com/photo-1518635017498-87f514b751ba?w=600",
+  },
+  {
+    id: 3,
+    name: "Organic Spinach",
+    category: "Leafy Greens",
+    price: 3.29,
+    image: "https://images.unsplash.com/photo-1576045057995-568f588f82fb?w=600",
+  },
+];
+
+export default products;
