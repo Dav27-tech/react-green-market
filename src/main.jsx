11d32f1e067c50409cd85ca67b9bbc05058b90f3
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-19 16:10:55 +0200

    Module: main - Updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
+import "./styles/index.css";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
