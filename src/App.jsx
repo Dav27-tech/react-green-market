b6d4d625cafceaa92b1eab4a20ac2b4a0e9322a2
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-19 16:08:41 +0200

    Module App - updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import Benefits from "./components/Benefits";
+import Products from "./components/Products";
 
 export default function App() {
   return (
@@ -8,6 +10,8 @@ export default function App() {
       <Navbar />
       <main>
         <Hero />
+        <Products />
+        <Benefits />
       </main>
       <Footer />
     </>
